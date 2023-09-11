d5d4b1a67020d4649158419e6df284d89d922ed8
AlexanderPonce13 <alexanderponceu.13@gmail.com>
2023-09-11 18:21:57 -0500

    Actualizado SECRET_KEY y configuración de la base de datos en settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y#di=lel7yoof%(743+qo7exq1^j%ik2229bfu2t)vodu3qv!_'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
